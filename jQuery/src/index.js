1c8530620f7793408c4dfb2b636dc84e16687562
Charles Vincent Cordial (DevExpress) <charles.cordial@devsoft.ph>
2025-11-28 16:28:12 +0800

    Fix: jQuery implementation lint and build v25.1.3

--- jQuery/src/index.js
+++ jQuery/src/index.js
@@ -1,10 +1,54 @@
+const firstUser = {
+  id: '1',
+  name: 'User',
+};
+
+const secondUser = {
+  id: '2',
+  name: 'Feedback Bot',
+  avatarUrl: '../../bot.png',
+};
+
+const initialMessages = [
+  {
+    timestamp: Date.now(),
+    author: secondUser,
+    text: "Hello! We'd love to hear your feedback. Please share your thoughts below!",
+  },
+];
+
+const alert = [
+  {
+    id: 1,
+    message: 'Session expired',
+  },
+];
+
 $(() => {
-  let count = 0;
-  $('#btn').dxButton({
-    text: `Click count: ${count}`,
-    onClick(e) {
-      count += 1;
-      e.component.option('text', `Click count: ${count}`);
-    },
-  });
+  function sendToBackend() {
+    setTimeout(() => {
+      chat.option('typingUsers', []);
+      chat.renderMessage({
+        text: 'Thanks for helping us improve!',
+        author: secondUser,
+        timestamp: Date.now(),
+      });
+      chat.option('alerts', alert);
+      chat.option('disabled', true);
+    }, 1000);
+  }
+
+  const chat = $('#chat')
+    .dxChat({
+      width: 400,
+      height: 450,
+      user: firstUser,
+      onMessageEntered: ({ component, message }) => {
+        component.renderMessage(message);
+        chat.option('typingUsers', [secondUser]);
+        sendToBackend();
+      },
+      items: initialMessages,
+    })
+    .dxChat('instance');
 });
